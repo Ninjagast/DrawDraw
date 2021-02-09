31bd35c3514e5d19719fc83b1326124125b849f8
grey <grey@gmail.com>
2021-02-09 14:39:44 +0100

    new project

--- drawDraw/Program.cs
+++ drawDraw/Program.cs
@@ -1,16 +1,9 @@
 ﻿using System;
 
-namespace drawDraw
+namespace DrawDraw
 {
-#if WINDOWS || LINUX
-    /// <summary>
-    /// The main class.
-    /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
@@ -18,5 +11,4 @@ namespace drawDraw
                 game.Run();
         }
     }
-#endif
 }
